7e915ff63aba0ace0e16ff31777cfce3d51d3894
ch04937 <97hernandez.c@gmail.com>
2020-01-15 15:46:35 -0800

    WIP: links the rooms

--- src/player.py
+++ src/player.py
@@ -1,2 +1,16 @@
 # Write a class to hold player information, e.g. what room they are in
 # currently.
+
+
+class Player:
+    def __init__(self, name, location):
+        self.name = name
+        self.location = location
+
+    def __str__(self):
+        return self.location
+
+
+# player = Player("Hero", "outside")
+
+# print(player)
--- src/room.py
+++ src/room.py
@@ -1,2 +1,22 @@
 # Implement a class to hold room information. This should have name and
 # description attributes.
+
+
+class Room:
+    def __init__(self, room, description):
+        self.room = room
+        self.description = description
+
+    def __str__(self):
+        return self.room
+
+
+# outside = Room("Outside Cave Entrance", "North of you, the cave mount beckons")
+
+# print(f"\n{outside.room} the description would be {outside.description}")
+
+
+# room = {
+#     'outside':  Room("Outside Cave Entrance", "North of you, the cave mount beckons"),
+# }
+# print(f"\n{room['outside']} {room['outside'].description}")
